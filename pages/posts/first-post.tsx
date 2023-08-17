662c4706c8f5792ed544460c9c8d71ceadd654e7
Gento <a551.return@gmail.com>
2023-08-17 19:39:41 +0900

    イメージとメタデータおよびCSSを扱う完了

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -7,19 +7,10 @@ import Layout from '@/components/layout';
 const FirstPost: FC = () => {
   return (
     <Layout>
-      <h1>First Post</h1>
-      <h2>
-        <Head>
-          <title>First Post</title>
-        </Head>
-        <Link href="/">← Back to home</Link>
-        <Image
-          src="/images/profile.jpg" // イメージファイルのルート
-          height={400} // イメージサイズの設定
-          width={400} // アスペクト比を正しく
-          alt="Your Name"
-        />
-      </h2>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Link href="/">← Back to home</Link>
     </Layout>
   );
 };
