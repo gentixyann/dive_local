90c29a6c03326a5c4c0c6b21c6809b98167d2687
Gento <a551.return@gmail.com>
2023-08-18 17:41:30 +0900

    APIキーを使ってgoogle mapの表示成功

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,18 @@
 import type { FC, ReactNode } from 'react';
 import styles from './layout.module.css';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 
-import utilStyles from '../styles/utils.module.css';
-
-const name = 'Your Name';
 export const siteTitle = 'Next.js Sample Website';
 type Props = {
   children: ReactNode;
-  home?: boolean;
 };
 
-const Layout: FC<Props> = ({ children, home }) => {
+const Layout: FC<Props> = ({ children }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
         <meta name="description" content="Learn how to build a personal website using Next.js" />
         <meta
           property="og:image"
@@ -28,45 +23,8 @@ const Layout: FC<Props> = ({ children, home }) => {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.png"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+
       <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      )}
     </div>
   );
 };
