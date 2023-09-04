f49fd41dbcf5a2f37f04012d7851f82c67ec041a
Gento <a551.return@gmail.com>
2023-09-04 09:05:03 +0900

    マーカーはMarkerFで使える

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
@@ -13,10 +13,23 @@ const center = {
 
 const zoom = 13;
 
+const positionAkiba = {
+  lat: 35.69731,
+  lng: 139.7747,
+};
+
+const positionIwamotocho = {
+  lat: 35.69397,
+  lng: 139.7762,
+};
+
 const Map = () => {
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={zoom}></GoogleMap>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={zoom}>
+        <MarkerF position={positionAkiba} onLoad={() => console.log('Marker Loaded')} />
+        <MarkerF position={positionIwamotocho} />
+      </GoogleMap>
     </LoadScript>
   );
 };
