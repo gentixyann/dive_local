3fc018146a97e1ba858f27024d9448d1799d7401
Gento <a551.return@gmail.com>
2023-09-05 23:22:30 +0900

    不要import削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from 'next/head';
 import Layout from '@/components/layout';
 import Map from '@/components/Map';
-import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
